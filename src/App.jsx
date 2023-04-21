3936e0d039ae66575fe191f5a1325169e03844fd
stekatag <stefangogov@protonmail.com>
2023-04-21 21:36:25 +0300

    Added navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import "./navigation.style.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <nav className="navigation">
+        <Link to="/" className="logo-container">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to="/" className="nav-link">
+            SHOP
+          </Link>
+        </div>
+      </nav>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
