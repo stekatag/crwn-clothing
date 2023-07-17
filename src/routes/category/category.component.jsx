b59f6862a1f7ab7c4e194bcefbe9d31f5d3e18ef
stekatag <stefangogov@proton.me>
2023-07-17 18:51:45 +0300

    Variables in useQuery and caching

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,23 +1,47 @@
-import { useContext, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { gql, useQuery } from "@apollo/client";
 
 import ProductCard from "../../components/product-card/product-card.component";
 import Spinner from "../../components/spinner/spinner.component";
-
-import { CategoriesContext } from "../../contexts/categories.context";
 
 import { Fragment } from "react";
 
 import { CategoryContainer, CategoryTitle } from "./category.style";
 
+const GET_CATEGORY = gql`
+  query ($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: { title: category },
+  });
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if (data) {
+      const {
+        getCollectionsByTitle: { items },
+      } = data;
+
+      setProducts(items);
+    }
+  }, [category, data]);
+
+  const [products, setProducts] = useState([]);
 
   return (
     <Fragment>
