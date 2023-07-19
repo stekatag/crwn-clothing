e97d6c330212305d089dfc5bf232b831bc7d999d
stekatag <stefangogov@proton.me>
2023-07-19 15:54:51 +0300

    PWA set up

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,17 @@ import { Elements } from "@stripe/react-stripe-js";
 import App from "./App";
 import { persistor, store } from "./store/store";
 import { stripePromise } from "./utils/stripe/stripe.utils";
+import { registerSW } from "virtual:pwa-register";
 
 import GlobalStyle from "./global.style";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New version available. Load new version?")) {
+      updateSW(true);
+    }
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
@@ -9,6 +10,51 @@ export default defineConfig({
     svgr({
       svgrOptions: {
         // svgr options
+      },
+    }),
+    VitePWA({
+      // add this to cache all the imports
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: ["**/*"],
+      manifest: {
+        theme_color: "#212121",
+        background_color: "#f7f7f7",
+        display: "browser",
+        scope: "/",
+        start_url: "/",
+        name: "Crwn Clothing",
+        short_name: "Crwn Clothing",
+        description: "A clothing store",
+        icons: [
+          {
+            src: "/crwn-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/crwn-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/crwn-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/crwn-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
     }),
   ],
