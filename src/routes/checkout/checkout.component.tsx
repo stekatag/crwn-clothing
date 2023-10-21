1ac644838d55b4888352986037d38a9b751bfbad
Stefan Gogov <stefangogov@proton.me>
2023-10-21 19:15:58 +0300

    Removed unused hook call

--- src/routes/checkout/checkout.component.tsx
+++ src/routes/checkout/checkout.component.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 import { useNavigate } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 import { useDetectAdblock } from "detect-adblock-react";
